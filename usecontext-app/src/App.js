3c9c9d14642b985ac777f03ab963698047e642d6
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2022-09-17 00:24:13 +0200

    dark mode use context

--- usecontext-app/src/App.js
+++ usecontext-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useContext, useCallback } from 'react';
+import React, { useState, useContext, useCallback, useMemo } from 'react';
 
 const THEMES = {
   dark: {
@@ -14,7 +14,68 @@ const THEMES = {
   },
 };
 
-const ThemeContext = React.createContext(THEMES.dark);
+//option 2
+// const ThemeContext = React.createContext(THEMES.dark);
+
+// const SearchForm = () => {
+//   return (
+//     <div>
+//       <input />
+//       <ThemedButton>Rechercher</ThemedButton>
+//     </div>
+//   );
+// };
+
+// const Toolbar = () => {
+//   return (
+//     <div>
+//       <SearchForm />
+//       <ThemedButton>M'inscrire</ThemedButton>
+//     </div>
+//   );
+// };
+// //option 1
+// // const ThemedButton = ({ children }) => {
+// //   return (
+// //     <ThemeContext.Consumer>
+// //       {(value) => {
+// //         return <button style={value}>{children}</button>;
+// //       }}
+// //     </ThemeContext.Consumer>
+// //   );
+// // };
+
+//
+// const ThemedButton = ({ children }) => {
+//   const value = useContext(ThemeContext);
+//   return <button style={value}>{children}</button>;
+// };
+
+// const App = () => {
+
+//   const [theme, setTheme] = useState('light');
+//   const toggleTheme = useCallback(() => {
+//     setTheme (t => t ==='light' ? 'dark': 'light')
+//   },[])
+//   const currentTheme = theme === 'light' ? THEMES.light : THEMES.dark
+
+//   return (
+//     <div>
+//       <ThemeContext.Provider value={currentTheme}>
+//         <Toolbar />
+//       </ThemeContext.Provider>
+//       <button onClick = {toggleTheme}>change theme</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//option 3
+const ThemeContext = React.createContext({
+  theme: THEMES.dark,
+  toggleTheme: () => {},
+});
 
 const SearchForm = () => {
   return (
@@ -33,38 +94,37 @@ const Toolbar = () => {
     </div>
   );
 };
-//option 1
-// const ThemedButton = ({ children }) => {
-//   return (
-//     <ThemeContext.Consumer>
-//       {(value) => {
-//         return <button style={value}>{children}</button>;
-//       }}
-//     </ThemeContext.Consumer>
-//   );
-// };
 
-//option 2
 const ThemedButton = ({ children }) => {
-  const value = useContext(ThemeContext);
-  return <button style={value}>{children}</button>;
+  const {theme} = useContext(ThemeContext);
+  return <button style={theme}>{children}</button>;
 };
 
 
-const App = () => { 
-
+const App = () => {
   const [theme, setTheme] = useState('light');
   const toggleTheme = useCallback(() => {
-    setTheme (t => t ==='light' ? 'dark': 'light')
-  },[])
-  const currentTheme = theme === 'light' ? THEMES.light : THEMES.dark
-
+    setTheme((t) => (t === 'light' ? 'dark' : 'light'));
+  }, []);
+  const value = useMemo(function() {
+    return {
+      theme: theme === 'light' ? THEMES.light : THEMES.dark,
+      toggleTheme
+    }
+  },[toggleTheme, theme]); 
+  
+  const ThemeSwitcher = () => {
+    const {toggleTheme} = useContext(ThemeContext)
+    return <button onClick={toggleTheme}>Change the theme</button>
+  }
+  
+  
   return (
     <div>
-      <ThemeContext.Provider value={currentTheme}>
+      <ThemeContext.Provider value={value}>
         <Toolbar />
+        <ThemeSwitcher />
       </ThemeContext.Provider>
-      <button onClick = {toggleTheme}>change theme</button>
     </div>
   );
 };
